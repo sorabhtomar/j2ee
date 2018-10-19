dae4dede764ff32c206c635fc1ef722fd8242307
Sorabh Tomar <sorabhtomar@users.noreply.github.com>
2018-10-19 18:56:54 +0530

    Deployed the First Servlet
    Tomcat Server Added and the first servlet was deployed

---
+++ tomcat/webapps/day1lab/src/HelloServlet.java
@@ -0,0 +1,40 @@
+//package dac;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+   @Override
+   public void doGet(HttpServletRequest request, HttpServletResponse response)
+         throws IOException, ServletException {
+ 
+      // Set the response MIME type of the response message
+      response.setContentType("text/html");
+      // Allocate a output writer to write the response message into the network socket
+      PrintWriter out = response.getWriter();
+ 
+      // Write the response message, in an HTML page
+      try {
+         out.println("<html>");
+         out.println("<head><title>Hello, World</title></head>");
+         out.println("<body>");
+         out.println("<h1>Hello, world!</h1>");  // says Hello
+         // Echo client's request information
+         out.println("<p>Request URI: " + request.getRequestURI() + "</p>");
+         out.println("<p>Protocol: " + request.getProtocol() + "</p>");
+         out.println("<p>PathInfo: " + request.getPathInfo() + "</p>");
+         out.println("<p>Remote Address: " + request.getRemoteAddr() + "</p>");
+         // Generate a random number upon each request
+         out.println("<p>A Random Number: <strong>" + Math.random() + "</strong></p>");
+         out.println("</body></html>");
+      } finally {
+         out.close();  // Always close the output writer
+      }
+   }
+}
